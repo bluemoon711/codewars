4fb48943aba4fe4684d9e1c1319197f4e75de838
Ying Fan <yingcathyfan@gmail.com>
2018-05-04 15:13:45 -0700

    add 2

--- Beeramid.js
+++ Beeramid.js
@@ -1,4 +1,18 @@
-var beeramid = function(bonus, price) {
+/* Let's pretend your company just hired your friend from college and paid you a referral bonus. Awesome!
+To celebrate, you're taking your team out to the terrible dive bar next door and using the referral 
+bonus to buy, and build, the largest three - dimensional beer can pyramid you can.And then probably
+drink those beers, because let's pretend it's Friday too.
+A beer can pyramid will square the number of cans in each level - 1 can in the top level, 4 in the
+second, 9 in the next, 16, 25...
+Complete the beeramid function to return the number of complete levels of a beer can pyramid you can
+make, given the parameters of:
+1) your referral bonus, and
+2) the price of a beer can
+For example:
+beeramid(1500, 2); // should === 12
+beeramid(5000, 3); // should === 16 */
+
+var beeramid = function (bonus, price) {
     var total = Math.floor(bonus/price);
     if (total < 1) return 0;
     if (total ==1) return 1;
@@ -23,7 +37,7 @@ const beeramid = (bonus, price) => {
     let maxBeer = Math.floor(bonus/price), level = 0, count = 0;
     while(count <= maxBeer) {
       count += Math.pow(level, 2);
-      level++
+      level++;
     }
     return level-2;
   }    
---
+++ RomanNumberalsDecoder.js
@@ -0,0 +1,58 @@
+/* Create a function that takes a Roman numeral as its argument and returns its value as a numeric
+decimal integer.You don't need to validate the form of the Roman numeral.
+Modern Roman numerals are written by expressing each decimal digit of the number to be encoded
+separately, starting with the leftmost digit and skipping any 0s.So 1990 is rendered "MCMXC"
+(1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII"(2000 = MM, 8 = VIII).The Roman numeral
+for 1666, "MDCLXVI", uses each letter in descending order.
+Example:
+solution('XXI'); // should return 21
+Help:
+Symbol    Value
+I          1
+V          5
+X          10
+L          50
+C          100
+D          500
+M          1,000 */
+
+function solution(roman) {
+  var data = { M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1 };
+  var numbers = roman.split('');
+  var sum = 0,
+    i;
+
+  for (i = 0; i < numbers.length; i++) {
+    if (data[numbers[i]] < data[numbers[i + 1]]) {
+      sum += data[numbers[i + 1]] - data[numbers[i]];
+      i++;
+    } else {
+      sum += data[numbers[i]];
+    }
+  }
+
+  return sum;
+}
+
+//alternative:
+function solution(roman) {
+  var conversion = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+  };
+
+  return roman
+    .match(/CM|CD|XC|XL|IX|IV|\w/g)
+    .reduce((accum, roman) => accum + conversion[roman], 0);
+}
